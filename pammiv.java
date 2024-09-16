722c5c9fe2a49cb76744e750d4cd43f4912a15c1
gouri <gouri@gmail.com>
2024-09-16 22:21:37 +0530

    adding files

---
+++ pammiv.java
@@ -0,0 +1,13 @@
+class Vardan
+{
+public static void main(String[] args){
+float p=55f;
+float c=42f;
+float m=45f;
+float o=60f;
+float sum=p+c+m+o;
+float per=(sum/280)*100;
+System.out.println(sum);
+System.out.println(per);
+}
+}
